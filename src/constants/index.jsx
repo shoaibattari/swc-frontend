9a27903233f4da8aa07e81b3a4e49031d8930b76
shoaib memon <shoaibattari83@gmail.com>
2025-12-27 19:51:43 +0500

    Update base URL for production environment

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:3001";
-// export const baseUrl = "https://ems-production-aff7.up.railway.app";
+// export const baseUrl = "http://localhost:3001";
+export const baseUrl = "https://ems-production-aff7.up.railway.app";
